311862dd53d83a4033c51550598ff16dd2f7cda3
KK2NJ <kk2boss007@gmail.com>
2025-11-25 16:18:18 -0500

    added files no name changes really or added any stuff really yet

---
+++ 3d_graphics_engine.srcs/sources_1/new/clk_wiz_0_clk_wiz.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/20/2025 01:44:22 PM
+-- Design Name: 
+-- Module Name: clk_wiz_0_clk_wiz - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity clk_wiz_0_clk_wiz is
+--  Port ( );
+end clk_wiz_0_clk_wiz;
+
+architecture Behavioral of clk_wiz_0_clk_wiz is
+
+begin
+
+
+end Behavioral;
